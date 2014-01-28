b8a9d8c9ed5db9b19f9c90ac9a5cf7da3185cd71
Ilya Drabenia <drobenyai@tut.by>
2014-01-29 01:05:59 +0300

    Add implementation of new localization approach

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,8 +7,29 @@ angular.module('benchmarkApp', [
 ])
   .run(function (gettextCatalog) {
     gettextCatalog.currentLanguage = 'ru';
+
+    
   })
-  .config(function ($routeProvider, $locationProvider) {
+  .factory('LocalizationInterceptor', function($q, gettextCatalog) {
+      return {
+       'request': function(config) {
+           return config;
+        },
+     
+        'response': function(response) {
+            _.templateSettings = {
+              'interpolate': /{{([\s\S]+?)}}/g
+            };
+            response.data = _.template(response.data, {t: gettextCatalog});
+            return response;
+        }
+      };
+  })
+  .config(function ($routeProvider, $locationProvider, $httpProvider) {
+
+    // alternatively, register the interceptor via an anonymous factory
+    $httpProvider.interceptors.push('LocalizationInterceptor');
+
     $routeProvider
       .when('/', {
         templateUrl: 'partials/main',
